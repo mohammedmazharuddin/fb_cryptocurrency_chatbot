f1eb9c1293e93e111c10f626e4a15fe870ece09a
Mohammed Mazhar Uddin <mohammed.mazhar666@gmail.com>
2022-03-14 23:06:24 -0400

    Add http utility module for performing http requests

---
+++ lib/fb_cryptocurrency_chatbot/helpers/http_utility.ex
@@ -0,0 +1,36 @@
+defmodule FbCryptocurrencyChatbot.HttpUtility do
+  
+  @moduledoc """
+  performs http operations
+  """
+  require Logger
+
+  def fetch(method, url, params, headers, options \\ [])
+  def fetch(:post, url, params, headers, options) do
+    HTTPoison.post(url, params, headers, options)
+    |> parse_response()
+  end
+
+  def fetch(:get, url, _, headers, options) do
+    HTTPoison.get(url, headers, options)
+    |> parse_response()
+  end
+
+  def fetch(method, url, body, headers, options) do
+    HTTPoison.request(method, url, body, headers, options)
+    |> parse_response()
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{} = response}) do
+    case response.status_code do
+      status when status in 200..204 -> {:ok, response}
+      404 -> {:error, :not_found}
+      422 -> {:error, :unprocessable_entity, Poison.decode!(response.body)}
+      _ -> {:error, :unprocessable_entity}
+    end
+  end
+
+  defp parse_response({:error, reason}) do
+    {:error, :unprocessable_entity, reason}
+  end
+end
