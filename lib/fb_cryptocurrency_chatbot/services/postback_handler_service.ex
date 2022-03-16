4fc8e0e3c1826694f5e212c36615136fbe86bd8d
Mohammed Mazhar Uddin <mohammed.mazhar666@gmail.com>
2022-03-16 01:08:30 -0400

    Add postback method to handle coins search

--- lib/fb_cryptocurrency_chatbot/services/postback_handler_service.ex
+++ lib/fb_cryptocurrency_chatbot/services/postback_handler_service.ex
@@ -4,19 +4,43 @@ defmodule FbCryptocurrencyChatbot.PostbackHandlerService do
   """
   alias FbCryptocurrencyChatbot.MessageSender
   alias FbCryptocurrencyChatbot.FbUtilityService
+  alias FbCryptocurrencyChatbot.CoinGeckoService
   require Logger
 
   def handle_postback("WELCOME", recipient) do
     text = "To search for coins, please use the options below."
     buttons_list = [{"Name", "COINS_BY_NAME"}, {"ID", "COINS_BY_ID"}]
+
     with \
       {:ok, username} <- FbUtilityService.fetch_user_name(recipient),
       {:ok, _resp} <- MessageSender.send(recipient, "Hello #{username.name}. Welcome to the world of crypto currency. We will assist you in finding the coins."),
       {:ok, _resp} <- FbUtilityService.send_button_options("postback", text, buttons_list, recipient)
+      do
+        :ok
+    else
+      _any -> Logger.error("Failed to handle 'WELCOME' postback.")
+    end
+  end
+
+  def handle_postback("COINS_BY_ID", recipient) do
+    with \
+      {:ok, coins} <- CoinGeckoService.fetch_coins(),
+      {:ok, _resp} <- FbUtilityService.send_generic_buttons_options("postback", "Select", coins, recipient) 
     do
       :ok
     else
-      _any -> Logger.error("Failed to handle 'WELCOME' postback.")
+      _any -> Logger.error("Failed to handle 'COINS_BY_ID' postback.")
+    end
+  end
+
+  def handle_postback("COINS_BY_NAME", recipient) do
+    with \
+      {:ok, coins} <- CoinGeckoService.fetch_coins(),
+      {:ok, _resp} <- FbUtilityService.send_generic_buttons_options("postback", "Select", coins, recipient) 
+    do
+      :ok
+    else
+      _any -> Logger.error("Failed to handle 'COINS_BY_NAME' postback.")
     end
   end
 
